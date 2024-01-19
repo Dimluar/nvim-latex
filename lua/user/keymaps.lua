3059a7eb7750e36711c7f5abb5c5486ff52fe165
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-01-19 20:37:03 +0100

    Update keymaps
    Added new keymaps and functions to make easier doing so.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,2 +1,28 @@
+local noremap = function(mode, keymap, action)
+    vim.keymap.set(mode, keymap, action, { noremap = true })
+end
+local keymap = function(mode, keymap, action)
+    vim.keymap.set(mode, keymap, action)
+end
+
 -- Exit to file tree
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap("n", "<leader>pv", vim.cmd.Ex)
+
+-- Center on vertical movement
+noremap("n", "<C-u>", "<C-u>zz")
+noremap("n", "<C-d>", "<C-d>zz")
+noremap("n", "n", "nzz")
+noremap("n", "N", "Nzz")
+
+-- Fast indent
+noremap("v", "<", "<v")
+noremap("v", ">", ">v")
+
+-- Move by displayed line
+noremap({"n", "v"}, "j", "gj")
+noremap({"n", "v"}, "k", "gk")
+noremap({"n", "v"}, "0", "g0")
+noremap({"n", "v"}, "$", "g$")
+
+-- Fast back to normal 
+noremap("i", "jf", "<Esc>")
