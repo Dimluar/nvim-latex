57ffb20cbfbd1747b0c02123fe56a37e7932cd42
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-01-20 22:47:55 +0100

    Add lsp configuration

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        opts = {},
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "texlab",
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            })
+            lspconfig.texlab.setup({})
+
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+}
