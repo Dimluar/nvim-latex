1e0c0ac0a1c1af27f9da8481bb09940112d4a976
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-01-21 17:41:33 +0100

    Update options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -3,25 +3,41 @@ vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.conceallevel = 3
+
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 vim.opt.wrap = true
+vim.opt.breakindent = true
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 vim.opt.clipboard = "unnamedplus"
-
-vim.opt.scrolloff = 999
-
+vim.opt.mouse = "a"
 vim.opt.virtualedit = "block"
-
 vim.opt.inccommand = "split"
 
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.hlsearch = false
 
-vim.opt.termguicolors = true 
+vim.opt.termguicolors = true
+
+vim.opt.spell = true
+vim.opt.spelllang = { "en" }
+
+vim.opt.undofile = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.hidden = true
+
+vim.opt.showmode = false
+vim.opt.updatetime = 750
