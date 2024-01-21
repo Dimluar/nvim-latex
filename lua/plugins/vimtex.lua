8b947ed77c9553655a286994c0fa864bb6325acc
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-01-21 19:20:35 +0100

    Change pdf viewer
    SumatraPDF doesn't support forward search on wsl.

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,11 +1,12 @@
 return {
 	"lervag/vimtex",
 	config = function()
-		vim.g.vimtex_view_general_viewer = "sumatraPDF"
-		vim.g.vimtex_view_general_options = "-reuse-instance @pdf"
-		vim.g.vimtex_view_general_options_latexmk = "-reuse-instance"
+		vim.g.vimtex_view_method = "sioyek"
+		vim.g.vimtex_view_sioyek_exe = "sioyek.exe"
+		vim.g.vimtex_callback_progpath = "wsl nvim"
+		vim.g.vimtex_view_sioyek_options = "--reuse-window --nofocus --forward-search-file @tex --forward-search-line @line"
 
-        vim.g.vimtex_syntax_enabled = 1
-        vim.g.vimtex_conceal_enabled = 1
-	end
+		vim.g.vimtex_syntax_enabled = 1
+		vim.g.vimtex_conceal_enabled = 1
+	end,
 }
