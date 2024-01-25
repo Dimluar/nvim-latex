44286e7676c679029301e8c18784f91c27d29e4e
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-01-25 19:33:26 +0100

    Set vimwiki workspaces

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,4 +1,9 @@
 require("user.options")
 require("user.keymaps")
 require("user.utils")
+
+vim.g.vimwiki_list = {
+  { path = "~/documents/notes/" },
+  { path = "~/documents/school/" },
+}
 require("user.lazy")
