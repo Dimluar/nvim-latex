6e3d4c4ddedb95bdd5f604a4ff26c7ae25afab2a
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-02-07 00:03:14 +0100

    Add keymaps to moves around windows

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,2 +1,18 @@
+local wk = require("which-key")
+
+-- Quality life
 vim.keymap.set("i", "jf", "<Esc>")
 vim.keymap.set("n", "-", vim.cmd.Ex)
+
+-- Window split
+wk.register({
+  ["<leader>"] = {
+    ["-"] = { "<Cmd>split<CR>", "Horizontal split" },
+    ["|"] = { "<Cmd>vsplit<CR>", "Vertical split" },
+  },
+})
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
