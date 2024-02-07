d21f95fe8577070c172be1ba54a5a3cf1b567e9f
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-02-07 18:37:44 +0100

    Add diagnostic icons

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,40 +1,46 @@
 return {
-  { "folke/neodev.nvim", opts = {}, },
-  { "williamboman/mason.nvim", opts = {}, },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local wk = require("which-key")
-      local lspconfig = require("lspconfig")
+	{ "folke/neodev.nvim", opts = {} },
+	{ "williamboman/mason.nvim", opts = {} },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local wk = require("which-key")
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
 
-      wk.register({
-        ["<leader>"] = {
-          l = {
-            name = "LSP",
-            h = { vim.lsp.buf.hover, "Hover documentation" },
-            a = { vim.lsp.buf.code_action, "Code actions" },
-            d = { vim.lsp.buf.definition, "Go to definition" },
-          },
-        },
-      })
-    end,
-  },
+			wk.register({
+				["<leader>"] = {
+					l = {
+						name = "LSP",
+						h = { vim.lsp.buf.hover, "Hover documentation" },
+						a = { vim.lsp.buf.code_action, "Code actions" },
+						d = { vim.lsp.buf.definition, "Go to definition" },
+					},
+				},
+			})
+
+			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+		end,
+	},
 }
