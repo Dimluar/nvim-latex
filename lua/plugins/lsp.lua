87f7dea4d89514d4417a99990815b7527984fa0e
Dimluar <148585902+Dimluar@users.noreply.github.com>
2024-02-10 22:37:59 +0100

    Install cmp plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,8 +14,10 @@ return {
 		config = function()
 			local wk = require("which-key")
 			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
 				settings = {
 					Lua = {
 						diagnostics = {
